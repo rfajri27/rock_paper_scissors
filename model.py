5ae5b5a0d0d5eae4ae45f889428b1c157381b2e1
Rahmat <rfajri255@gmail.com>
2021-09-16 13:17:59 +0700

    update and add some file

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
-from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
 physical_device = tf.config.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_device[0], True)
 
@@ -30,13 +32,6 @@ def rps_model():
                                                                   class_mode='categorical',
                                                                   target_size=(150, 150))
 
-    class myCallback(tf.keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs={}):
-            if (logs.get('accuracy') >= 0.99 and logs.get('val_accuracy') >= 0.9):
-                self.model.stop_training = True
-
-    callbacks = myCallback()
-
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
@@ -52,22 +47,43 @@ def rps_model():
         tf.keras.layers.Dense(3, activation='softmax')
     ])
 
-    optimizer = tf.optimizers.RMSprop(lr=0.001)
+    optimizer = tf.optimizers.Adam(lr=0.0001)
     model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(train_generator,
+    epoch = 50
+    H = model.fit(train_generator,
               steps_per_epoch=20,
-              epochs=50,
+              epochs=epoch,
               validation_data=validation_generator,
-              validation_steps=3,
-              callbacks=[callbacks])
+              validation_steps=3)
 
-    return model
+    return model, H, epoch
 
 # Save model as .h5 file
 if __name__ == '__main__':
-    model = rps_model()
+    model, H, epoch = rps_model()
     model.save("model.h5")
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, epoch), H.history["loss"], label="training")
+plt.plot(np.arange(0, epoch), H.history["val_loss"], label="validation")
+plt.title("Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("images/plot.png")
+
+plt.figure()
+plt.plot(np.arange(0, epoch), H.history["accuracy"], label="training")
+plt.plot(np.arange(0, epoch), H.history["val_accuracy"], label="validation")
+plt.title("Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.savefig("images/plot_acc.png")
+
+
 
